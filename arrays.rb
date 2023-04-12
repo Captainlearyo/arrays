b11acc0bafc7fd02bc1f2b939ac2066274ae3b10
Captainlearyo <clayoleary@gmail.com>
2023-04-12 15:18:29 -0700

    Add methods

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,25 @@
 strings = ["1", "2","3","4"]
+puts strings.pop
+# pop removes and returns the last element of an array
+
 integers = [1, 2, 3, 4]
+
+puts integers.shift
+#shift removes and returns the first element 
+
+puts integers.unshift(5)
+# unshift adds and element to the beggining of an array
+puts p integers
+
 floats = [1.1, 1.2, 1.3, 1.4]
+
+puts floats[3]
+
 booleans = [true, false, true , false]
+
+puts booleans.push(true)
+#push adds to the end of an array
+p booleans
+
+puts integers.length
+# legnth returns the length of an array
